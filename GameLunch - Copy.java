df2e1e89398cd3c97320f356e4739454967bc64d
nitesh Kumar jain <niteshkrjain2004@gmail.com>
2023-03-30 17:49:55 +0530

    guesser game

---
+++ GameLunch - Copy.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class guesser
+{
+   int guessfromguesser;
+   int guesser()
+   {
+    Scanner sc = new Scanner(System.in);
+       System.out.println("Guesser guess the Number ");
+       guessfromguesser=sc.nextInt();
+       return guessfromguesser;
+   }
+}class playler()
+{
+    Scanner sc = new Scanner(System.in);
+    int guessfromplayer;
+    int player()
+    {
+        System.out.println("player guess the number");
+        guessfromplayer = sc.nextInt();
+        return guessfromplayer;
+    }
+
+}
+class
+public class GameLunch {
+
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+}
